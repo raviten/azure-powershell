c84599814b116accb027be924ea7b122202c0c5f
Ravi Teja <ranandu@microsoft.com>
2019-08-28 10:11:38 +0530

    moving User to Users Folder

--- src/DataBoxEdge/DataBoxEdge/Common/Constants.cs
+++ src/DataBoxEdge/DataBoxEdge/Common/Constants.cs
@@ -16,6 +16,7 @@ namespace Microsoft.Azure.PowerShell.Cmdlets.DataBoxEdge.Common
         public const string ExtendedInfo = AzurePrefix + SERVICE_NAME + "Device" + "ExtendedInfo";
         public const string User = AzurePrefix + SERVICE_NAME + "User";
         public const string Sac = AzurePrefix + SERVICE_NAME + "SAC";
+        public const string Role = AzurePrefix + SERVICE_NAME + "Role";
         public const string Share = AzurePrefix + SERVICE_NAME + "Share";
         public const string BandwidthSchedule = AzurePrefix + SERVICE_NAME + "BandwidthSchedule";
         public const string Test = AzurePrefix + SERVICE_NAME + "Test";
